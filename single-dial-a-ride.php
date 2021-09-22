f3655420e3875f28a8b424fbafcccfa33420f0aa
estelle <e.keaton@live.com>
2021-09-22 08:06:25 -0700

    cleaning up dar cpt

--- single-dial-a-ride.php
+++ single-dial-a-ride.php
@@ -2,30 +2,28 @@
 /*
 Template Name: route_individual_page
 */
- get_header(); ?>
+ 			get_header(); ?>
 
-			<style>
-				body { margin:0; padding:0; }
-				#map { width:100%; height: 450px;}
-			</style>
+			
 					<?php get_template_part( 'route-header'); ?> 
 			
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					 
 				<div class="header-container">	
 					<h1 id="route-page-title" class="over-blue col-sm-9" >
-						<i id="icon-lrg-<?php the_field('hex-color'); ?>" class="route-icon"></i><?php the_title() ?></h1>
+						<?php the_title() ?>
+					</h1>
 					
 					
 						<div id="route-select-container" class="col-sm-3">
 					
-						<?php darSelect(); ?>
+							<?php darSelect(); ?>
 						
 					
-					</div><!-- end #route-select-container -->
+						</div><!-- end #route-select-container -->
 				</div><!-- class="header-container" -->
 				<div id="generic-wide-container" class="row-fluid">
-					<div id="dar-info-container" style="padding: 0;" class="col-sm-4" >
+					<div id="dar-info-container" class="col-sm-4" >
 							<div id="dar-cities">
 								<?php the_field('cities');  ?>
 							</div><!-- end #dar-cities -->
@@ -36,25 +34,30 @@ Template Name: route_individual_page
 									<?php  the_field('times_of_day'); ?>
 							</div><!-- end #dar-times-of-day -->
 							<div id="dar-info">
+								<div class="fare-header"><a href="<?php echo get_site_url(); ?>/fares">Fares</a></div>
+								<?php if (have_rows('fares')): ?>
 									<table id="single-route-fares">
-										<tr>
-											<th></th>
-											<th>General</th>
-											<th>Reduced</th>
-										</tr>
-										<tr>
-											<td class="mini-label"><a href="<?php echo get_site_url(); ?>/fares">Fare</a></td>
-											<td>$2.00</td>
-											<td>$1.00</td>
-										</tr>
+										<tbody>
+										<?php
+											while (have_rows('fares')): the_row(); ?>
+											<tr>
+												<th><?php the_sub_field('fare_type')?></th>
+												<td><?php the_sub_field('fare_price'); ?></td>
+												
+											</tr>
+										</tbody>
+										<?php endwhile; ?>
 									</table>
+									<?php else :
+										printf('No Fares');
+									endif; ?>
 							</div><!-- end #dar-info -->
 					</div><!-- end #dar-info-container -->
 
 		 
 
 					<div id="dar-map-container" class="col-sm-8" style="padding: 0;" >
-						<div id='map'>
+						<div id='dar-map'>
 							<?php if (get_field('custom_imap_url')) : ?>
 								<iframe src="<?php wp_specialchars_decode(the_field('custom_imap_url')); ?>"></iframe>
 							<?php else : ?>
